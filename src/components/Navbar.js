46a0f0da641444e60c3e1f6a55bf5c1826c68e05
Mohammad Abdul Lateef <mohammad@Mohammads-MacBook-Air.local>
2022-12-23 02:52:45 +0530

    added active class using useLocation in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,10 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React, {useEffect} from 'react'
+import { Link, useLocation} from 'react-router-dom';
 
 
 export default function Navbar() {
+    let location = useLocation();
+    useEffect(()=>{}, [location]);
     return (
         <>
         <div className='container navber-container'>
@@ -14,12 +16,12 @@ export default function Navbar() {
 
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav ms-auto nav">
-                        <li className="nav-item"><Link to="/" className="nav-link nav-text">Home</Link></li>
-                        <li className="nav-item"><Link to="/Bazar" className="nav-link nav-text">Bazaar</Link></li>
-                        <li className="nav-item"><Link to="/Products" className="nav-link nav-text">Products</Link></li>
-                        <li className="nav-item"><Link to="/" className="nav-link nav-text">CBS</Link></li>
-                        <li className="nav-item"><Link to="/" className="nav-link nav-text">Helpers</Link></li>
-                        <li className="nav-item"><Link to="/" className="nav-link nav-text">Contact Us</Link></li>
+                        <li className="nav-item"><Link to="/" className={`nav-link nav-text ${location.pathname==='/'?'active':''}`}>Home</Link></li>
+                        <li className="nav-item"><Link to="/Bazar" className={`nav-link nav-text ${location.pathname==='/Bazar'?'active':''}`}>Bazaar</Link></li>
+                        <li className="nav-item"><Link to="/Products" className={`nav-link nav-text ${location.pathname==='/Products'?'active':''}`}>Products</Link></li>
+                        <li className="nav-item"><Link to="/" className={`nav-link nav-text ${location.pathname==='/CBS'?'active':''}`}>CBS</Link></li>
+                        <li className="nav-item"><Link to="/" className={`nav-link nav-text ${location.pathname==='/Helpers'?'active':''}`}>Helpers</Link></li>
+                        <li className="nav-item"><Link to="/" className={`nav-link nav-text ${location.pathname==='/ContactUs'?'active':''}`}>Contact Us</Link></li>
                         <li><img id="cart-img" src={require("../images/illustrations/cart.png")} alt='cart' /></li>
                         <li className="nav-item"><Link to="/login" className="nav-link"><button type="button" className="btn btn-outline-success nav-text">Sign In</button></Link></li>
                     </ul>
